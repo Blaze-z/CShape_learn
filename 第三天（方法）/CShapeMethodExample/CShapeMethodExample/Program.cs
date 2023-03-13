bbf88f647cc4e756bba99d6e696845c92d2d2f79
blaze_z <3246536418@qq.com>
2023-03-13 21:59:51 +0800

    init

---
+++ 第三天（方法）/CShapeMethodExample/CShapeMethodExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShapeMethodExample
+{
+    internal class Program
+    {
+
+        /*
+         方法（函数）的申明与使用
+         */
+        static void Main(string[] args)
+        {
+            //函数的使用
+            Console.WriteLine(Calculator.GetCircularArea(10));
+            Console.WriteLine("=============================");
+            Console.WriteLine(Calculator.GetCylinderVolume(10,20));
+            Console.WriteLine("=============================");
+            Console.WriteLine(Calculator.GetConeVolume(10,20));
+        }
+
+
+        //申明一个类
+        class Calculator
+        {
+
+            //申明类的方法（函数）。
+            public static double GetCircularArea(double r)
+            {
+                return Math.PI * r * r;
+            }
+            public static double GetCylinderVolume(double r, double h)
+            {
+                return GetCircularArea(r) * h;
+            }
+            public static double GetConeVolume(double r, double h)
+            {
+                return GetCylinderVolume(r, h) / 3;
+            }
+        }
+    }
+}
---
+++ 第三天（方法）/ConstructorExample/ConstructorExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *当函数有多个重载（overload）时，会自动选择一个合适的进行执行。
+             */
+            //第一个构造函数创造的实例
+            Student stu1 = new Student(10, "Mr.Okey");
+            Console.WriteLine(stu1.ID);
+            Console.WriteLine(stu1.Name);
+            Console.WriteLine("==================");
+            //第二个构造函数创造的实例
+            Student stu2 = new Student();
+            Console.WriteLine(stu2.ID);
+            Console.WriteLine(stu2.Name);
+
+        }
+
+        class Student
+        {
+
+            //构造函数快捷键ctor
+            public Student(int intiID,string intiName) 
+            {
+                this.ID = intiID;
+                this.Name = intiName;
+            }
+
+
+            //方法的重载（overload)
+            public Student()
+            {
+                this.ID = 1;
+                this.Name = "No Name";               
+            }
+            public int ID;
+            public string Name;
+        }
+    }
+}
